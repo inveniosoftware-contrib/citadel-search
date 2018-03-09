978082ac35aa81d79d3456571843113c979b8b97
Pablo Panero <pablo.panero@cern.ch>
2018-03-09 15:57:31 +0100

    Add passwd file editing to create-instance.sh script according to OpenShift doc

--- scripts/create-instance.sh
+++ scripts/create-instance.sh
@@ -5,3 +5,13 @@ set -o nounset
 
 mkdir -p ${INVENIO_INSTANCE_PATH}
 pip install -r requirements.txt
+pip install -e .[all,elasticsearch5]
+
+# Needed for getpwuid() from python
+# Doc https://docs.openshift.org/latest/creating_images/guidelines.html#openshift-origin-specific-guidelines
+# At "Support Arbitrary User IDs"
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
