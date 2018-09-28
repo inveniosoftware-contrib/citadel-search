d97b0627635bd855cdd3d65e64927d25f7ac288b
Pablo Panero <pablo.panero@cern.ch>
2018-09-28 09:26:39 +0200

    Bug Fix: config variable name should not contain env prefix

--- cern_search_rest_api/modules/cernsearch/utils.py
+++ cern_search_rest_api/modules/cernsearch/utils.py
@@ -41,10 +41,10 @@ def cern_search_record_to_index(record):
                     return index_name, index_name[len(prefix) + 1:]
 
     current_app.logger.debug('Index {0}{1} - Doc {2}'.format(
-        current_app.config['CERN_SEARCH_INDEX_PREFIX'],
+        current_app.config['INDEX_PREFIX'],
         current_app.config['INDEXER_DEFAULT_INDEX'],
         current_app.config['INDEXER_DEFAULT_DOC_TYPE'])
     )
-    return ('{0}{1}'.format(current_app.config['CERN_SEARCH_INDEX_PREFIX'],
+    return ('{0}{1}'.format(current_app.config['INDEX_PREFIX'],
                             current_app.config['INDEXER_DEFAULT_INDEX']),
             current_app.config['INDEXER_DEFAULT_DOC_TYPE'])
