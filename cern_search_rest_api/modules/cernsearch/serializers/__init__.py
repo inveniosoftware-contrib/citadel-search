a56ff71a4982830e1f0450e53da9572ebfa0d227
Pablo Panero <pablo.panero@cern.ch>
2019-01-14 10:10:22 +0100

    Add serializer and increase records-rest version to include the latest pid_checker

---
+++ cern_search_rest_api/modules/cernsearch/serializers/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018, CERN
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+from __future__ import absolute_import, print_function
+
+from invenio_records_rest.serializers.json import JSONSerializer
+from invenio_records_rest.serializers.response import record_responsify, \
+    search_responsify
+
+from cern_search_rest_api.modules.cernsearch.marshmallow import \
+    CSASRecordSchemaV1
+
+# Serializers
+# ===========
+#: JSON serializer definition.
+
+json_v1 = JSONSerializer(CSASRecordSchemaV1, replace_refs=True)
+
+# Records-REST serializers
+# ========================
+#: JSON record serializer for individual records.
+json_v1_response = record_responsify(json_v1, 'application/json')
+#: JSON record serializer for search results.
+json_v1_search = search_responsify(json_v1, 'application/json')
+
+__all__ = (
+    'json_v1',
+    'json_v1_response',
+    'json_v1_search',
+)
