9780f56746cdbd7e9fe90fff2923c8700306ec32
Pablo Panero <pablo.panero@cern.ch>
2019-04-15 09:50:43 +0200

    [skip ci] license: add license to tests

--- tests/test_schema_validation.py
+++ tests/test_schema_validation.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Search.
+# Copyright (C) 2018-2019 CERN.
+#
+# CERN Search is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
 import json
 
 import pytest
