fe969e571bc8b4a1555628e54ac6e9f6df2ecf0b
Pablo Panero <pablo.panero@cern.ch>
2019-04-15 09:53:40 +0200

    [skip ci] license: add license to scripts

--- scripts/patch/oauth_patch.sh
+++ scripts/patch/oauth_patch.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Search.
+# Copyright (C) 2018-2019 CERN.
+#
+# CERN Search is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 location=$(pip show invenio-oauthclient | grep Location | awk '{print $2}')
 
--- scripts/populate-instance.sh
+++ scripts/populate-instance.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# This file is part of CERN Search.
+# Copyright (C) 2018-2019 CERN.
+#
+# CERN Search is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
 
 invenio db init
 invenio db create
