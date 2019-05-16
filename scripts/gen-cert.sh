021cd5f3e4e3d1b8ac77a053d4477b2a5086653d
Adrian <adrian.moennich@cern.ch>
2019-05-16 10:52:32 +0200

    Simplify gen-cert.sh
    No need to run all those commands; openssl can do it with a single one :)

--- scripts/gen-cert.sh
+++ scripts/gen-cert.sh
@@ -7,9 +7,6 @@
 # CERN Search is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-openssl genrsa -des3 -passout pass:x -out nginx.pass.key 2048
-openssl rsa -passin pass:x -in nginx.pass.key -out nginx.key
-rm nginx.pass.key
-openssl req -new -key nginx.key -out nginx.csr \
-  -subj "/C=CH/ST=Geneve/L=Geneve/O=CERN/OU=IT Department/CN=Search as a Service"
-openssl x509 -req -days 365 -in nginx.csr -signkey nginx.key -out nginx.crt
+openssl req -x509 -nodes -newkey rsa:4096 \
+  -subj '/C=CH/ST=Geneve/L=Geneve/O=CERN/OU=IT Department/CN=Search as a Service' \
+  -keyout nginx.key -out nginx.crt
